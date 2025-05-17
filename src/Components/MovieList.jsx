bdf1145e1369b1c4689c202e724060ba04c600f3
soyeon1351 <pack6411351@gmail.com>
2025-05-17 13:51:05 +0900

    ✨ 영화 목록 컴포넌트 구현
    TMDB API에서 인기 영화 목록을 받아와 MovieList 컴포넌트에서 포스터, 제목, 평점을 카드 형태로 렌더링함

--- src/Components/MovieList.jsx
+++ src/Components/MovieList.jsx
@@ -1,5 +1,38 @@
+import { useEffect, useState } from "react";
+import { TMDB_IMAGE_URL } from "../Config";
+import { TMDB_BASE_URL } from "../Config";
+import { TOKEN } from "../Config";
+
 export default function MovieList () {
+    const [movies, setMovies] = useState([])
+    
+    useEffect(() => {
+        const options = {
+            method: 'GET',
+            headers: {
+                accept: 'application/json',
+                Authorization: `Bearer ${TOKEN}`
+            }
+        };
+        
+        fetch(`${TMDB_BASE_URL}/movie/popular?language=ko-KR&page=1`, options)
+        .then(res => res.json())
+        .then(res => setMovies(res.results))
+        .catch(err => console.error(err));
+    },[])
+
     return (
-        <p>무비리스트</p>
+        <div>
+        {movies.map((movie)=>(
+            <div key = {movie.id}>
+            <img 
+                src={`${TMDB_IMAGE_URL}${movie.poster_path}`}
+                alt={movie.title}
+            />
+            <h2>{movie.title}</h2>
+            <p>⭐️ {movie.vote_average}</p>
+            </div>
+))}
+        </div>
     )
 }
