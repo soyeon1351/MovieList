66d07c7d8d702e822cad9070312ec9645a9f2a3d
soyeon1351 <pack6411351@gmail.com>
2025-05-19 23:54:31 +0900

    💄 무비 카드 레이아웃 SCSS 정리 및 4열 배치 적용
    SCSS를 사용하여 무비 카드들을 한 줄에 4개씩 정렬, 이미지 크기에 max-width 제한을 두어 너무 커지지 않도록 설정, 텍스트 정렬 및 여백 간격 조정으로 전체 레이아웃 개선

--- src/Components/MovieList.jsx
+++ src/Components/MovieList.jsx
@@ -1,3 +1,4 @@
+import '../Components/MovieList.scss'
 import { useEffect, useState } from "react";
 import { TMDB_IMAGE_URL } from "../Config";
 import { TMDB_BASE_URL } from "../Config";
@@ -19,6 +20,7 @@ export default function MovieList () {
             try {
             const res = await fetch(`${TMDB_BASE_URL}/movie/popular?language=ko-KR&page=1`, options)
             const data = await res.json()
+            console.log(data)
 
             const filteredMovies = data.results.filter((movie)=> movie.adult === false)
                 setMovies(filteredMovies)
@@ -30,15 +32,16 @@ export default function MovieList () {
     },[])
 
     return (
-        <div>
+        <div className='movieCard'>
         {movies.map((movie)=>(
-            <div key = {movie.id}>
+            <div key = {movie.id} className='card'>
             <img 
+                className='img'
                 src={`${TMDB_IMAGE_URL}${movie.poster_path}`}
                 alt={movie.title}
             />
-            <h2>{movie.title}</h2>
-            <p>⭐️ {movie.vote_average}</p>
+            <h2 className='title'>{movie.title}</h2>
+            <p className='vote'>⭐️ {movie.vote_average}</p>
             </div>
 ))}
         </div>
