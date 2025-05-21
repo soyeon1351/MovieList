9609b0d8c0d65f7ecca70aa7f83999a68f7167d4
soyeon1351 <pack6411351@gmail.com>
2025-05-21 12:29:34 +0900

    🎨 스타일: NavBar 레이아웃 및 버튼 스타일링 적용

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,13 +1,17 @@
 import { Link } from "react-router-dom"
+import '../Components/Navbar.scss'
 
 export default function NavBar () {
     return (
-        <div>
-            <Link to="/">포멜로무비</Link>
-            <input type="text"/>
+        <div className="navbar">
+            <Link to="/" className="logo-link">
+            <img className="pomelo" src="src/포멜로.png" alt="이미지"/>
+            <span className="logo-text">포멜로무비</span>
+            </Link>
+            <input type="text" placeholder="검색어를 입력하세요." className="input-text"/>
             <div>
-                <button>로그인</button>
-                <button>회원가입</button>
+                <button className="login-btn">로그인</button>
+                <button className="signup-btn">회원가입</button>
             </div>
         </div>
     )
