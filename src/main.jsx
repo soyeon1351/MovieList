2678d2d4a748d203483553c33ae5a8f967d0a978
soyeon1351 <pack6411351@gmail.com>
2025-05-26 23:26:47 +0900

    user 상태 context API 생성

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,16 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import { SupabaseProvider } from './supabase/index.js'
+import { UserProvider } from './supabase/context/UserContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <UserProvider>
     <SupabaseProvider>
       <BrowserRouter>
         <App />
       </BrowserRouter>
     </SupabaseProvider>
+    </UserProvider>
   </StrictMode>
 )
